0bb1d829f8c315691124db168d8f269c550ff4bb
Rui Tian <tr.tianrui@foxmail.com>
2022-09-01 15:16:21 +0800

    Split XHGNet datasets with a ratio of 10%

--- split_data.py
+++ split_data.py
@@ -5,58 +5,55 @@ import random
 
 def mk_file(file_path: str):
     if os.path.exists(file_path):
-        # 如果文件夹存在，则先删除原文件夹在重新创建
+        # if file_path exist, delete it firstly
         rmtree(file_path)
     os.makedirs(file_path)
 
 
 def main():
-    # 保证随机可复现
+    # set pseudo-random seed for reproductivity
     random.seed(0)
 
-    # 将数据集中10%的数据划分到验证集中
+    # 10% validation datasets
     split_rate = 0.1
 
-    # 指向你解压后的flower_photos文件夹
-    data_root = os.getcwd()
-    origin_flower_path = os.path.join(data_root, "Datasets")
-    assert os.path.exists(origin_flower_path), "path '{}' does not exist.".format(origin_flower_path)
+    # root/to/ImageFolder
+    data_root = os.path.join(os.getcwd(), "..", "XHGNet")
+    origin_image_path = os.path.join(data_root, "train-val")
+    assert os.path.exists(origin_image_path), "path '{}' does not exist.".format(origin_image_path)
 
-    flower_class = [cla for cla in os.listdir(origin_flower_path)
-                    if os.path.isdir(os.path.join(origin_flower_path, cla))]
+    image_class = [cls for cls in os.listdir(origin_image_path)
+                    if os.path.isdir(os.path.join(origin_image_path, cls))]
 
-    # 建立保存训练集的文件夹
+    # mkdir(empty) for `train` datasets
     train_root = os.path.join(data_root, "train")
     mk_file(train_root)
-    for cla in flower_class:
-        # 建立每个类别对应的文件夹
-        mk_file(os.path.join(train_root, cla))
+    for cls in image_class:
+        mk_file(os.path.join(train_root, cls))
 
-    # 建立保存验证集的文件夹
+    # mkdir(empty) for `val` datasets
     val_root = os.path.join(data_root, "val")
     mk_file(val_root)
-    for cla in flower_class:
-        # 建立每个类别对应的文件夹
-        mk_file(os.path.join(val_root, cla))
+    for cls in image_class:
+        mk_file(os.path.join(val_root, cls))
 
-    for cla in flower_class:
-        cla_path = os.path.join(origin_flower_path, cla)
-        images = os.listdir(cla_path)
+    for cls in image_class:
+        cls_path = os.path.join(origin_image_path, cls)
+        images = os.listdir(cls_path)
         num = len(images)
-        # 随机采样验证集的索引
-        eval_index = random.sample(images, k=int(num*split_rate))
+        eval_index = random.sample(images, k=int(num*split_rate))  # random sampling by index
         for index, image in enumerate(images):
             if image in eval_index:
-                # 将分配至验证集中的文件复制到相应目录
-                image_path = os.path.join(cla_path, image)
-                new_path = os.path.join(val_root, cla)
+                # copy tree to `val` datasets
+                image_path = os.path.join(cls_path, image)
+                new_path = os.path.join(val_root, cls)
                 copy(image_path, new_path)
             else:
-                # 将分配至训练集中的文件复制到相应目录
-                image_path = os.path.join(cla_path, image)
-                new_path = os.path.join(train_root, cla)
+                # copy tree to `train` datasets
+                image_path = os.path.join(cls_path, image)
+                new_path = os.path.join(train_root, cls)
                 copy(image_path, new_path)
-            print("\r[{}] processing [{}/{}]".format(cla, index+1, num), end="")  # processing bar
+            print("\r[{}] processing [{}/{}]".format(cls, index+1, num), end="")  # processing bar
         print()
 
     print("processing done!")

ec2082a3c6d9ec2ef5d4d87f1e9ff5db331f2b2c
Tian Rui <tr.tianrui@foxmail.com>
2022-10-12 14:28:11 +0800

    feat: ShuffleNet_v2_x0_5 with XHGNet

--- Test7_shufflenet/train.py
+++ Test7_shufflenet/train.py
@@ -1,131 +1,121 @@
 import os
-import math
 import argparse
 
 import torch
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
-import torch.optim.lr_scheduler as lr_scheduler
 
-from model import shufflenet_v2_x1_0
 from my_dataset import MyDataSet
-from utils import read_split_data, train_one_epoch, evaluate
+from utils import read_split_data, create_lr_scheduler, get_params_groups, train_one_epoch, evaluate
+# Load torchvision models
+from torchvision.models import shufflenet_v2_x0_5 as create_model, ShuffleNet_V2_X0_5_Weights
+# from torchvision.models import shufflenet_v2_x1_0 as create_model, ShuffleNet_V2_X1_0_Weights
+# from torchvision.models import shufflenet_v2_x1_5 as create_model, ShuffleNet_V2_X1_5_Weights
+# from torchvision.models import shufflenet_v2_x2_0 as create_model, ShuffleNet_V2_X2_0_Weights
 
 
 def main(args):
+    
+    # ---> Device Config <---
     device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    print(f"using {device} device.")
 
-    print(args)
-    print('Start Tensorboard with "tensorboard --logdir=runs", view at http://localhost:6006/')
+    if os.path.exists("./save_weights") is False:
+        os.makedirs("./save_weights")
+
     tb_writer = SummaryWriter()
-    if os.path.exists("./weights") is False:
-        os.makedirs("./weights")
+    
 
+    # ---> Prepare Datasets <---
     train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
 
+    img_size = 224
     data_transform = {
-        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+        "train": transforms.Compose([transforms.RandomResizedCrop(size=img_size, scale=(0.8, 0.83), ratio=(0.98, 1.02)),
                                      transforms.RandomHorizontalFlip(),
                                      transforms.ToTensor(),
                                      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
-        "val": transforms.Compose([transforms.Resize(256),
-                                   transforms.CenterCrop(224),
+        "val": transforms.Compose([transforms.CenterCrop((2048, 2048)),
+                                   transforms.Resize((224, 224)),
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 
-    # 实例化训练数据集
-    train_dataset = MyDataSet(images_path=train_images_path,
-                              images_class=train_images_label,
-                              transform=data_transform["train"])
-
-    # 实例化验证数据集
-    val_dataset = MyDataSet(images_path=val_images_path,
-                            images_class=val_images_label,
-                            transform=data_transform["val"])
+    train_dataset = MyDataSet(images_path=train_images_path, images_class=train_images_label, transform=data_transform["train"])
+    val_dataset = MyDataSet(images_path=val_images_path, images_class=val_images_label, transform=data_transform["val"])
 
     batch_size = args.batch_size
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=batch_size,
-                                               shuffle=True,
-                                               pin_memory=True,
-                                               num_workers=nw,
-                                               collate_fn=train_dataset.collate_fn)
 
-    val_loader = torch.utils.data.DataLoader(val_dataset,
-                                             batch_size=batch_size,
-                                             shuffle=False,
-                                             pin_memory=True,
-                                             num_workers=nw,
-                                             collate_fn=val_dataset.collate_fn)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True,
+                                               num_workers=nw, collate_fn=train_dataset.collate_fn)
 
-    # 如果存在预训练权重则载入
-    model = shufflenet_v2_x1_0(num_classes=args.num_classes).to(device)
-    if args.weights != "":
-        if os.path.exists(args.weights):
-            weights_dict = torch.load(args.weights, map_location=device)
-            load_weights_dict = {k: v for k, v in weights_dict.items()
-                                 if model.state_dict()[k].numel() == v.numel()}
-            print(model.load_state_dict(load_weights_dict, strict=False))
-        else:
-            raise FileNotFoundError("not found weights file: {}".format(args.weights))
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True,
+                                             num_workers=nw, collate_fn=val_dataset.collate_fn)
 
-    # 是否冻结权重
+    weights = ShuffleNet_V2_X0_5_Weights.DEFAULT
+    model = create_model(weights=weights)
+    in_features = model.fc.in_features
+    model.fc = torch.nn.Linear(in_features, args.num_classes)
+    model.to(device=device)
+    # model.classifier.add_module('68-head', torch.nn.Linear(1000, args.num_classes))
+
     if args.freeze_layers:
         for name, para in model.named_parameters():
-            # 除最后的全连接层外，其他权重全部冻结
-            if "fc" not in name:
+            # 除head外，其他权重全部冻结
+            if "head" not in name:
                 para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
 
-    pg = [p for p in model.parameters() if p.requires_grad]
-    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=4E-5)
-    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
-    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
-    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+    # pg = [p for p in model.parameters() if p.requires_grad]
+    pg = get_params_groups(model, weight_decay=args.wd)
+    optimizer = optim.AdamW(pg, lr=args.lr, weight_decay=args.wd)
+    lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), args.epochs, warmup=True, warmup_epochs=1)
 
+    best_acc = 0.
     for epoch in range(args.epochs):
         # train
-        mean_loss = train_one_epoch(model=model,
-                                    optimizer=optimizer,
-                                    data_loader=train_loader,
-                                    device=device,
-                                    epoch=epoch)
-
-        scheduler.step()
+        train_loss, train_acc = train_one_epoch(model=model,
+                                                optimizer=optimizer,
+                                                data_loader=train_loader,
+                                                device=device,
+                                                epoch=epoch,
+                                                lr_scheduler=lr_scheduler)
 
         # validate
-        acc = evaluate(model=model,
-                       data_loader=val_loader,
-                       device=device)
+        val_loss, val_acc = evaluate(model=model,
+                                     data_loader=val_loader,
+                                     device=device,
+                                     epoch=epoch)
 
-        print("[epoch {}] accuracy: {}".format(epoch, round(acc, 3)))
-        tags = ["loss", "accuracy", "learning_rate"]
-        tb_writer.add_scalar(tags[0], mean_loss, epoch)
-        tb_writer.add_scalar(tags[1], acc, epoch)
-        tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch)
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
 
-        torch.save(model.state_dict(), "./weights/model-{}.pth".format(epoch))
+        if best_acc < val_acc:
+            torch.save(model.state_dict(), "save_weights/best_model.pth")
+            best_acc = val_acc
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=5)
-    parser.add_argument('--epochs', type=int, default=30)
-    parser.add_argument('--batch-size', type=int, default=16)
-    parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--lrf', type=float, default=0.1)
+    parser.add_argument('--num_classes', type=int, default=68)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batch-size', type=int, default=256)
+    parser.add_argument('--lr', type=float, default=5e-4)
+    parser.add_argument('--wd', type=float, default=5e-2)
 
-    # 数据集所在根目录
-    # https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz
-    parser.add_argument('--data-path', type=str,
-                        default="/data/flower_photos")
+    parser.add_argument('--data-path', type=str, default="../data/XHGNet/train")
 
-    # shufflenetv2_x1.0 官方权重下载地址
-    # https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth
-    parser.add_argument('--weights', type=str, default='./shufflenetv2_x1.pth',
+    # load pretrain model on ImageNet, don't load if set to ""
+    parser.add_argument('--weights', type=str, default='',
                         help='initial weights path')
+    # whether freeze layers except cls-head
     parser.add_argument('--freeze-layers', type=bool, default=False)
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
 

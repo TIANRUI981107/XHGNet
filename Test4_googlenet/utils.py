c179dd07414a4af8c1df0566bb4859c5da19d615
Tian Rui <tr.tianrui@foxmail.com>
2022-10-16 12:56:20 +0800

    fix: Inception_v3 with XHGNet

--- Test4_googlenet/utils.py
+++ Test4_googlenet/utils.py
@@ -16,17 +16,21 @@ import numpy as np
 def read_split_data(root: str, val_rate: float = 0.2):
 
     # pseudo-random seed for reproductivity
-    random.seed(0)  
+    random.seed(0)
     assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
 
     # ImageFolder format
-    image_class = [cls for cls in os.listdir(root) if os.path.isdir(os.path.join(root, cls))]
+    image_class = [
+        cls for cls in os.listdir(root) if os.path.isdir(os.path.join(root, cls))
+    ]
     image_class.sort()
 
     # 生成类别名称以及对应的数字索引
     class_indices = dict((k, v) for v, k in enumerate(image_class))
-    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
-    with open('class_indices.json', 'w') as json_file:
+    json_str = json.dumps(
+        dict((val, key) for key, val in class_indices.items()), indent=4
+    )
+    with open("class_indices.json", "w") as json_file:
         json_file.write(json_str)
 
     train_images_path = []  # 存储训练集的所有图片路径
@@ -41,8 +45,11 @@ def read_split_data(root: str, val_rate: float = 0.2):
         cls_path = os.path.join(root, cls)
 
         # 遍历获取supported支持的所有文件路径
-        images = [os.path.join(root, cls, i) for i in os.listdir(cls_path)
-                  if os.path.splitext(i)[-1] in supported]
+        images = [
+            os.path.join(root, cls, i)
+            for i in os.listdir(cls_path)
+            if os.path.splitext(i)[-1] in supported
+        ]
         # 获取该类别对应的索引
         image_index = class_indices[cls]
         # 记录该类别的样本数量
@@ -69,19 +76,19 @@ def read_split_data(root: str, val_rate: float = 0.2):
     if plot_image:
         try:
             # 绘制每种类别个数柱状图
-            plt.bar(range(len(image_class)), every_class_num, align='center')
+            plt.bar(range(len(image_class)), every_class_num, align="center")
             # 将横坐标0,1,2,3,4替换为相应的类别名称
             plt.xticks(range(len(image_class)), image_class, rotation=45, fontsize=8)
             # 在柱状图上添加数值标签
             for i, v in enumerate(every_class_num):
-                plt.text(x=i, y=v + 5, s=str(v), ha='center', fontsize=8)
+                plt.text(x=i, y=v + 5, s=str(v), ha="center", fontsize=8)
             # 设置x坐标
-            plt.xlabel('image class')
+            plt.xlabel("image class")
             # 设置y坐标
-            plt.ylabel('number of images')
+            plt.ylabel("number of images")
             # 设置柱状图的标题
-            plt.title('Class Distribution')
-            plt.savefig("./Classes_Distr.png", dpi=600, bbox_inches='tight')
+            plt.title("Class Distribution")
+            plt.savefig("./Classes_Distr.png", dpi=600, bbox_inches="tight")
             plt.show()
         except Exception as e:
             print(e)
@@ -95,7 +102,11 @@ def read_split_data(root: str, val_rate: float = 0.2):
             cls_index_series = pd.Series(image_class, name="Class")
             cls_distr_series = pd.Series(every_class_num, name="Number-of-Images")
             cls_distr_df = pd.concat([cls_index_series, cls_distr_series], axis=1)
-            cls_distr_df.to_csv("./class_distributions.csv", index=False, columns=["Class", "Number-of-Images"])
+            cls_distr_df.to_csv(
+                "./class_distributions.csv",
+                index=False,
+                columns=["Class", "Number-of-Images"],
+            )
             print("./cls_distri_series.csv saved successfully!")
         except Exception as e:
             print(e)
@@ -108,9 +119,9 @@ def plot_data_loader_image(data_loader):
     batch_size = data_loader.batch_size
     plot_num = min(batch_size, 4)
 
-    json_path = './class_indices.json'
+    json_path = "./class_indices.json"
     assert os.path.exists(json_path), json_path + " does not exist."
-    json_file = open(json_path, 'r')
+    json_file = open(json_path, "r")
     class_indices = json.load(json_file)
 
     for data in data_loader:
@@ -121,21 +132,21 @@ def plot_data_loader_image(data_loader):
             # 反Normalize操作
             img = (img * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255
             label = labels[i].item()
-            plt.subplot(1, plot_num, i+1)
+            plt.subplot(1, plot_num, i + 1)
             plt.xlabel(class_indices[str(label)])
             plt.xticks([])  # 去掉x轴的刻度
             plt.yticks([])  # 去掉y轴的刻度
-            plt.imshow(img.astype('uint8'))
+            plt.imshow(img.astype("uint8"))
         plt.show()
 
 
 def write_pickle(list_info: list, file_name: str):
-    with open(file_name, 'wb') as f:
+    with open(file_name, "wb") as f:
         pickle.dump(list_info, f)
 
 
 def read_pickle(file_name: str) -> list:
-    with open(file_name, 'rb') as f:
+    with open(file_name, "rb") as f:
         info_list = pickle.load(f)
         return info_list
 
@@ -144,7 +155,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
     model.train()
     loss_function = torch.nn.CrossEntropyLoss()
     accu_loss = torch.zeros(1).to(device)  # 累计损失
-    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
     optimizer.zero_grad()
 
     sample_num = 0
@@ -153,7 +164,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
         images, labels = data
         sample_num += images.shape[0]
 
-        pred = model(images.to(device))
+        pred, _ = model(images.to(device))
         pred_classes = torch.max(pred, dim=1)[1]
         accu_num += torch.eq(pred_classes, labels.to(device)).sum()
 
@@ -161,15 +172,17 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
         loss.backward()
         accu_loss += loss.detach()
 
-        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
-            epoch,
-            accu_loss.item() / (step + 1),
-            accu_num.item() / sample_num,
-            optimizer.param_groups[0]["lr"]
+        data_loader.desc = (
+            "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
+                epoch,
+                accu_loss.item() / (step + 1),
+                accu_num.item() / sample_num,
+                optimizer.param_groups[0]["lr"],
+            )
         )
 
         if not torch.isfinite(loss):
-            print('WARNING: non-finite loss, ending training ', loss)
+            print("WARNING: non-finite loss, ending training ", loss)
             sys.exit(1)
 
         optimizer.step()
@@ -186,7 +199,7 @@ def evaluate(model, data_loader, device, epoch):
 
     model.eval()
 
-    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
     accu_loss = torch.zeros(1).to(device)  # 累计损失
 
     sample_num = 0
@@ -203,21 +216,21 @@ def evaluate(model, data_loader, device, epoch):
         accu_loss += loss
 
         data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(
-            epoch,
-            accu_loss.item() / (step + 1),
-            accu_num.item() / sample_num
+            epoch, accu_loss.item() / (step + 1), accu_num.item() / sample_num
         )
 
     return accu_loss.item() / (step + 1), accu_num.item() / sample_num
 
 
-def create_lr_scheduler(optimizer,
-                        num_step: int,
-                        epochs: int,
-                        warmup=True,
-                        warmup_epochs=1,
-                        warmup_factor=1e-3,
-                        end_factor=1e-6):
+def create_lr_scheduler(
+    optimizer,
+    num_step: int,
+    epochs: int,
+    warmup=True,
+    warmup_epochs=1,
+    warmup_factor=1e-3,
+    end_factor=1e-6,
+):
     assert num_step > 0 and epochs > 0
     if warmup is False:
         warmup_epochs = 0
@@ -232,22 +245,28 @@ def create_lr_scheduler(optimizer,
             # warmup过程中lr倍率因子从warmup_factor -> 1
             return warmup_factor * (1 - alpha) + alpha
         else:
-            current_step = (x - warmup_epochs * num_step)
+            current_step = x - warmup_epochs * num_step
             cosine_steps = (epochs - warmup_epochs) * num_step
             # warmup后lr倍率因子从1 -> end_factor
-            return ((1 + math.cos(current_step * math.pi / cosine_steps)) / 2) * (1 - end_factor) + end_factor
+            return ((1 + math.cos(current_step * math.pi / cosine_steps)) / 2) * (
+                1 - end_factor
+            ) + end_factor
 
     return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=f)
 
 
 def get_params_groups(model: torch.nn.Module, weight_decay: float = 1e-5):
     # 记录optimize要训练的权重参数
-    parameter_group_vars = {"decay": {"params": [], "weight_decay": weight_decay},
-                            "no_decay": {"params": [], "weight_decay": 0.}}
+    parameter_group_vars = {
+        "decay": {"params": [], "weight_decay": weight_decay},
+        "no_decay": {"params": [], "weight_decay": 0.0},
+    }
 
     # 记录对应的权重名称
-    parameter_group_names = {"decay": {"params": [], "weight_decay": weight_decay},
-                             "no_decay": {"params": [], "weight_decay": 0.}}
+    parameter_group_names = {
+        "decay": {"params": [], "weight_decay": weight_decay},
+        "no_decay": {"params": [], "weight_decay": 0.0},
+    }
 
     for name, param in model.named_parameters():
         if not param.requires_grad:
